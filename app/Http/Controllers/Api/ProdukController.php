9f2d46d1d58399c45ccb0fab798976d913b7e28e
bataviel1997 <luqmansa66@gmail.com>
2021-11-05 20:42:03 +0700

    1.upload pertama lemanfarmweb

---
+++ app/Http/Controllers/Api/ProdukController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use\App\Produk;
+
+class ProdukController extends Controller
+{
+    
+    public function index() {
+
+        // dd($request->all());die();
+        // code mengambil semua produk dalam database API
+        $produk = Produk::all();
+          return response()->json([
+            'success' => 1,
+            'message' => 'Get Produk Berhasil ',
+            'produks' => $produk
+            ]);
+    }
+}
